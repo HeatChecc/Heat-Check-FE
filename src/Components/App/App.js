46fd11081af3db795ab2003d7b99c3efd96547d1
Ivy Nguyen <animeweirdo24@gmail.com>
2022-10-15 13:45:51 -0600

    render resturants near an address

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import Homepage from '../Homepage/Homepage';
 import './App.css';
-import { Route, BrowserRouter, Routes } from "react-router-dom";
+import { Route, BrowserRouter, Routes} from "react-router-dom";
 import ResturantsContainer from '../ResturantsContainer/ReviewsContainer'
 import Resturant from '../Resturant/Resturant';
 import Dish from '../Dish/Dish';
 
 const App = () => {
+
   return (
     <div className="App">
       <BrowserRouter>
       <Routes>
       <Route exact path="/" element={<Homepage />}/>
-      <Route path="/search/:lat/:long" element={<ResturantsContainer />}/>
+      <Route path="/search/:id" element={<ResturantsContainer />}/>
       <Route path="/resturant/:id" element={<Resturant />}/>
       <Route path="/dish/:id" element={<Dish />}/>
       </Routes>
--- src/Components/ResturantsContainer/ReviewsContainer.js
+++ src/Components/ResturantsContainer/ReviewsContainer.js
@@ -1,11 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./ReviewsContainer.css"
-import { useParams } from 'react-router-dom'
+// import Resturant from '../Resturant/Resturant'
+import { Link, useParams } from 'react-router-dom'
 
 const ReviewsContainer = () => {
-  let {lat, long} = useParams();
+  let {id} = useParams();
+  const [resturants, setResturants] = useState([])
+
+  const fetchResturants = () => {
+    let myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer dJEWPcptSi0S89Fq0IOr6VU9OSmNVpfAP-L4Xmr0U3fNtUty7b2PeRmQylJCH_QGk5dcq2lUdlIt-juVbw4De3V9dPToVlq_7lT3kal84w1b3PPz1ytGx1es6vlKY3Yx");
+
+    let raw = "";
+
+    let requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+      // body: raw,
+      redirect: 'follow'
+    };
+
+    fetch(`https://arcane-hollows-12884.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=\"spicy\"&location=\"${id}\"`, requestOptions)
+      .then(response => response.json())
+      .then(result => {
+        console.log(result)
+        setResturants(result.businesses)
+      })
+      .catch(error => console.log('error', error));
+  }
+
+  useEffect(() => {
+    fetchResturants()
+  }, [])
+
+  const resturantCards = resturants.map(resturant => {
+    const {name, id, image_url, rating} = resturant
+    return <div>
+              <img src={image_url}/>
+              <Link to={`/resturant/${id}`}> {name} </Link>
+              <p>{rating}</p>
+          </div>
+  })
+
   return (
-    <div>ReviewsContainer. This will one day do a call utilizing the latitude: {lat} and longitude: {long}</div>
+    <div>
+      ReviewsContainer. This will one day do a call utilizing the id: {resturants.length}
+      {resturantCards}
+    </div>
   )
 }
 
