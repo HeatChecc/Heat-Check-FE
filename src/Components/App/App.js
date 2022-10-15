94f911832b9ad975e00470f7a1d5c0c1e159d057
David Daw <daviddaw@gmail.com>
2022-10-15 10:34:06 -0700

    Installs react router

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Homepage from '../Homepage/Homepage';
 import './App.css';
+import { Route, BrowserRouter, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="App">
-      <Homepage/>
+      <BrowserRouter>
+      <Routes>
+      <Route exact path="/" element={<Homepage />}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
