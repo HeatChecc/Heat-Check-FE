4fee089b31b0470f810ddce0cea6fe773af2fe46
Ivy Nguyen <animeweirdo24@gmail.com>
2022-10-15 18:52:26 -0600

    Add styling to restaurant list page and details page

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <div className='heatCheckHeader'>
+        <h2 id="heat">HEAT</h2>
+        <h2 id="check">CHECK</h2>
+    </div>
+  )
+}
+
+export default Header
