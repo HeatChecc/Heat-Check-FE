35dfb36934d246e5770c15b794b8efcb856f721a
David Daw <daviddaw@gmail.com>
2022-10-19 15:21:06 -0700

    Add graphql version of restaurants call

--- src/index.js
+++ src/index.js
@@ -7,23 +7,12 @@ import { BrowserRouter } from 'react-router-dom';
 import { ApolloClient, InMemoryCache, ApolloProvider, gql} from '@apollo/client'
 
 const client = new ApolloClient({
-  uri: 'https://flyby-gateway.herokuapp.com/',
+  uri: 'https://heatcheck-be.herokuapp.com/graphql',
   cache: new InMemoryCache(),
 })
 
-client.query({
-    query: gql`
-      query GetLocations {
-        locations {
-          id
-          name
-          description
-          photo
-        }
-      }
-    `,
-  })
-  .then((result) => console.log(result));
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
