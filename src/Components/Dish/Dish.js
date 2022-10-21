dfb2289187dd44b0266d6b37d54bd52a6034ce15
Ivy Nguyen <animeweirdo24@gmail.com>
2022-10-20 19:54:36 -0600

    remove gql from dish.js

--- src/Components/Dish/Dish.js
+++ src/Components/Dish/Dish.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { gql, useQuery} from '@apollo/client'
+import {useQuery} from '@apollo/client'
 import "./Dish.css"
 import { useParams } from 'react-router-dom'
 import Loading from '../Loading/Loading'
