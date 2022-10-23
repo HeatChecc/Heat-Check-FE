c33c9356579eb938b7aa4d9cafa0c02697f418dd
Cleveland Ticoalu <ayocleveland@gmail.com>
2022-10-22 19:54:31 -0600

    Add team member gh hyperlinks

--- src/Components/AboutHeatCheck/AboutHeatCheck.js
+++ src/Components/AboutHeatCheck/AboutHeatCheck.js
@@ -4,17 +4,20 @@ import "./AboutHeatCheck.css"
 const AboutHeatCheck = () => {
   return (
     <div className='teams'>
+
+        <h1> About Heat Check </h1>
+        <p> Looking for two burns? </p>
+
         <h1> Front End Team </h1>
-        <ul> Ivy Nguyen </ul>
-        <ul> David Daw </ul>
-        <ul> Cleveland Ticoalu</ul>
-        <ul> Ivy Nguyen</ul>
+        <ul> Ivy Nguyen <a href="https://github.com/INguyen22" rel="noreferrer"> GitHub </a> </ul>
+        <ul> David Daw <a href="https://github.com/davidhdaw" rel="noreferrer"> GitHub </a> </ul>
+        <ul> Cleveland Ticoalu <a href="https://github.com/cleveland231" rel="noreferrer"> GitHub </a> </ul>
 
         <h1> Back End Team </h1>
-        <ul> Phillip Stewerts </ul>
-        <ul> Eli Sachs </ul>
-        <ul> Gauri Joshi </ul>
-        <ul> Ethan Nguyen </ul>
+        <ul> Phillip Stewerts <a href="https://github.com/philmarcu" rel="noreferrer"> GitHub </a> </ul>
+        <ul> Eli Sachs <a href="https://github.com/easachs" rel="noreferrer"> GitHub </a> </ul>
+        <ul> Gauri Joshi <a href="https://github.com/gaurijo" rel="noreferrer"> GitHub </a> </ul>
+        <ul> Ethan Nguyen <a href="https://github.com/Ethan-t-n" rel="noreferrer"> GitHub </a> </ul>
     </div>
   )
 }
