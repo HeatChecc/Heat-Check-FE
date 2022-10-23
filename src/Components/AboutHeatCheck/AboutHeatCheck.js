48507d18df9e4c45a5be519ac32bd8d20c83d2d3
Cleveland Ticoalu <ayocleveland@gmail.com>
2022-10-22 19:44:57 -0600

    Set up AboutHeatCheck component

---
+++ src/Components/AboutHeatCheck/AboutHeatCheck.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./AboutHeatCheck.css"
+
+const AboutHeatCheck = () => {
+  return (
+    <div className='teams'>
+        <h1> Front End Team </h1>
+        <ul> Ivy Nguyen </ul>
+        <ul> David Daw </ul>
+        <ul> Cleveland Ticoalu</ul>
+        <ul> Ivy Nguyen</ul>
+
+        <h1> Back End Team </h1>
+        <ul> Phillip Stewerts </ul>
+        <ul> Eli Sachs </ul>
+        <ul> Gauri Joshi </ul>
+        <ul> Ethan Nguyen </ul>
+    </div>
+  )
+}
+
+export default AboutHeatCheck
