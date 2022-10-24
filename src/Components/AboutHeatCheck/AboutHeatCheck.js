0655c88c1a6e42d4514e86d65aa7ebeaa5293d0e
Cleveland Ticoalu <ayocleveland@gmail.com>
2022-10-24 12:07:58 -0600

    Add linkedin hyperlink to AboutHeatCheck component

--- src/Components/AboutHeatCheck/AboutHeatCheck.js
+++ src/Components/AboutHeatCheck/AboutHeatCheck.js
@@ -12,17 +12,17 @@ const AboutHeatCheck = () => {
             <div className='teams'>
                 <div className='teams-style'>
                     <h1> Front End Team </h1>
-                    <ul> Ivy Nguyen <a href="https://github.com/INguyen22" rel="noreferrer"> GitHub </a> </ul>
-                    <ul> David Daw <a href="https://github.com/davidhdaw" rel="noreferrer"> GitHub </a> </ul>
-                    <ul> Cleveland Ticoalu <a href="https://github.com/cleveland231" rel="noreferrer"> GitHub </a> </ul>
+                    <ul> Ivy Nguyen <a href="https://github.com/INguyen22" rel="noreferrer"> GitHub </a> <a href="https://www.linkedin.com/in/ivy-nguyen-051b27212/" rel="noreferrer"> LinkedIn </a> </ul>
+                    <ul> David Daw <a href="https://github.com/davidhdaw" rel="noreferrer"> GitHub </a> <a href="https://www.linkedin.com/in/david-daw-04aa36237/" rel="noreferrer"> LinkedIn </a> </ul>
+                    <ul> Cleveland Ticoalu <a href="https://github.com/cleveland231" rel="noreferrer"> GitHub </a> <a href="https://www.linkedin.com/in/cleveland-ticoalu/" rel="noreferrer"> LinkedIn </a> </ul>
                 </div>
 
                 <div className='teams-style'>
                 <h1> Back End Team </h1>
-                    <ul> Phillip Stewerts <a href="https://github.com/philmarcu" rel="noreferrer"> GitHub </a> </ul>
-                    <ul> Eli Sachs <a href="https://github.com/easachs" rel="noreferrer"> GitHub </a> </ul>
-                    <ul> Gauri Joshi <a href="https://github.com/gaurijo" rel="noreferrer"> GitHub </a> </ul>
-                    <ul> Ethan Nguyen <a href="https://github.com/Ethan-t-n" rel="noreferrer"> GitHub </a> </ul>
+                    <ul> Phillip Stewerts <a href="https://github.com/philmarcu" rel="noreferrer"> GitHub </a> <a href="https://www.linkedin.com/in/phillip-stewart-15497183/" rel="noreferrer"> LinkedIn </a> </ul>
+                    <ul> Eli Sachs <a href="https://github.com/easachs" rel="noreferrer"> GitHub </a> <a href="https://www.linkedin.com/in/easachs/" rel="noreferrer"> LinkedIn </a> </ul>
+                    <ul> Gauri Joshi <a href="https://github.com/gaurijo" rel="noreferrer"> GitHub </a> <a href="https://www.linkedin.com/in/gaurijo/" rel="noreferrer"> LinkedIn </a> </ul>
+                    <ul> Ethan Nguyen <a href="https://github.com/Ethan-t-n" rel="noreferrer"> GitHub </a> <a href="https://www.linkedin.com/in/ethan-nguyen-82b398233/" rel="noreferrer"> LinkedIn </a> </ul>
                 </div>
             </div>
         </div>
